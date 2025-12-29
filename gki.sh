74b7ecccd88a3887733b637bf8ec9d112ab51687
Naifi <247718176+rufnx@users.noreply.github.com>
2025-12-29 14:58:08 +0700

    Update gki.sh

--- gki.sh
+++ gki.sh
@@ -9,26 +9,26 @@ fetch_kernel_common() {
   GKI_ROOT=$(pwd)/gki-kernel
   local BRANCH GKI_ROOT
 
-  _log "Target kernel branch: $BRANCH"
+  _log "Target kernel branch: ${BRANCH}"
 
-  if [ -d $GKI_ROOT/.repo ]; then
+  if [ -d "${GKI_ROOT}/.repo" ]; then
     _log "Kernel repo already exists, skipping repo init"
   else
-    _log "Preparing workspace: $GKI_ROOT"
-    mkdir $GKI_ROOT
-    cd $GKI_ROOT
+    _log "Preparing workspace: ${GKI_ROOT}"
+    mkdir -p "${GKI_ROOT}"
+    cd "${GKI_ROOT}"
 
     _log "[1/2] repo init"
     repo init \
       --depth=1 \
       -u https://android.googlesource.com/kernel/manifest \
-      -b "$BRANCH"
+      -b "${BRANCH}"
 
     _log "Pause 5s (network / auth sanity check)"
     sleep 5
   fi
 
-  cd $GKI_ROOT
+  cd "${GKI_ROOT}"
 
   _log "[2/2] repo sync (this may take a while)"
   repo sync \
